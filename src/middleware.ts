5abbcdadbe64539396ffa56690cd67c38c0718fc
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2024-03-07 00:24:38 -0800

    middleware for finding the local and directing the user to the right language

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,36 @@
-import { NextResponse } from "next/server";
- 
-let locales = ['en-US', 'nl-NL', 'nl']
- 
+import { NextRequest, NextResponse } from "next/server";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+
+let locales = ["en", "arb"];
+let headers = { "accept-language": "en-US,en;q=0.5" };
+let languages = new Negotiator({ headers }).languages();
+let defaultLocale = "en-US";
+
 // Get the preferred locale, similar to the above or using a library
-function getLocale(request) { ... }
- 
-export function middleware(request) {
+const getLocale = (request: NextRequest) => {
+  return match(languages, locales, defaultLocale); // -> 'en-US'
+};
+
+export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
-  const { pathname } = request.nextUrl
+  const { pathname } = request.nextUrl;
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  )
- 
-  if (pathnameHasLocale) return
- 
+  );
+  if (pathnameHasLocale) return;
   // Redirect if there is no locale
-  const locale = getLocale(request)
-  request.nextUrl.pathname = `/${locale}${pathname}`
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
   // e.g. incoming request is /products
   // The new URL is now /en-US/products
-  return NextResponse.redirect(request.nextUrl)
+  return NextResponse.redirect(request.nextUrl);
 }
- 
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
-    '/((?!_next).*)',
+    "/((?!_next).*)",
     // Optional: only run on root (/) URL
     // '/'
   ],
-}
+};
