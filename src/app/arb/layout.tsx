2a70a52808658e64c8fac988ffcf66cb4be7c57f
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2024-03-07 23:49:36 -0800

    arabic and en main layouts

--- src/app/arb/layout.tsx
+++ src/app/arb/layout.tsx
@@ -1,10 +1,5 @@
 import NavBar from "@/components/arb/NavBar";
-import { Footer } from "@/components/arb/Footer";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-// needs to be changed to arabic
-const inter = Inter({ subsets: ["latin"] });
+import { FooterComponent } from "@/components/arb/FooterComponent";
 
 export default function RootLayout({
   children,
@@ -12,12 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="arb">
-      <body className={inter.className}>
-        <NavBar />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <>
+      <NavBar />
+      {children}
+      <FooterComponent />
+    </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,16 +5,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-  params: { lang },
 }: Readonly<{
   children: React.ReactNode;
-  params: { lang: string };
 }>) {
-  console.log(lang);
-
   return (
     <html>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
