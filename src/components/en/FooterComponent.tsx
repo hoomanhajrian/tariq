c5875555b1c3a079fa43c3ee1137722977a4889e
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2024-03-08 01:00:39 -0800

    temperory footers

--- src/components/en/FooterComponent.tsx
+++ src/components/en/FooterComponent.tsx
@@ -7,13 +7,16 @@ import {
 
 export const FooterComponent = () => {
   return (
-    <Footer container>
-      <FooterCopyright href="#" by="Flowbite™" year={2022} />
+    <Footer container className="absolute bottom-0 left-0 bg-black">
+      <FooterCopyright
+        href="https://hh-portfolio.com"
+        by="Hooman"
+        year={2022}
+      />
       <FooterLinkGroup>
-        <FooterLink href="#">About</FooterLink>
-        <FooterLink href="#">Privacy Policy</FooterLink>
-        <FooterLink href="#">Licensing</FooterLink>
-        <FooterLink href="#">Contact</FooterLink>
+        <FooterLink href="#">Phone: +1(778)-751-1620</FooterLink>
+        <FooterLink href="#">Email: tariqlouiss@yahoo.com</FooterLink>
+        <FooterLink href="#">Facebook: tariq louis</FooterLink>
       </FooterLinkGroup>
     </Footer>
   );
