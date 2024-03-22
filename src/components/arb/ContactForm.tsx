b6db1aa51916cd5bc5e158b70b5cf1eaeccc5eca
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2024-03-22 14:02:52 -0700

    contact form with different languages

--- src/components/arb/ContactForm.tsx
+++ src/components/arb/ContactForm.tsx
@@ -1,29 +1,64 @@
-import { Button, Label, Select, TextInput, ToggleSwitch } from "flowbite-react";
+"use client";
+import { useState, useId } from "react";
+import { Button, Label, Textarea, TextInput } from "flowbite-react";
+import { MuiTelInput } from "mui-tel-input";
+
 const ContactForm = () => {
+  const [formData, updateFormData] = useState({
+    fullName: "",
+    phone: "",
+    email: "",
+    request: "",
+  });
+
+  const nameId = useId();
+  const phoneId = useId();
+  const emailId = useId();
+  const requestId = useId();
+
   return (
-    <form className="flex max-w-md flex-col gap-4">
+    <form className="flex max-w-md flex-col gap-4 bg-orange p-10 mt-10 mb-10 mr-auto ml-auto rounded-xl">
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="email1" value="Your email" />
+          <Label htmlFor={nameId} value="اسمك بالكامل :" />
         </div>
-        <TextInput
-          id="email1"
-          type="email"
-          placeholder="name@flowbite.com"
-          required
+        <TextInput id={nameId} type="text" required />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor={emailId} value="ایمیل :" />
+        </div>
+        <TextInput id={emailId} type="email" required />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor={phoneId} value="رقم التليفون :" />
+        </div>
+        <MuiTelInput
+          style={{
+            backgroundColor: "white",
+            borderRadius: "12px",
+            width: "100%",
+          }}
+          id={phoneId}
+          placeholder="تغيير البلد إذا لزم الأمر"
+          langOfCountryName="arb"
+          size="small"
+          variant="outlined"
+          defaultCountry="IQ"
+          value={formData.phone}
+          onChange={(value: any) => {
+            updateFormData({ ...formData, phone: value });
+          }}
         />
       </div>
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="password1" value="Your password" />
+          <Label htmlFor={requestId} value="طلب :" />
         </div>
-        <TextInput id="password1" type="password" required />
+        <Textarea id={requestId} maxLength={250} />
       </div>
-      <div className="flex items-center gap-2">
-        <Select></Select>
-        <Label htmlFor="remember">Remember me</Label>
-      </div>
-      <Button type="submit">Submit</Button>
+      <Button type="submit">يُقدِّم</Button>
     </form>
   );
 };
--- src/components/en/ContactForm.tsx
+++ src/components/en/ContactForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useId } from "react";
+import { useState, useId, useEffect } from "react";
 import { Button, Label, Textarea, TextInput } from "flowbite-react";
 import { MuiTelInput } from "mui-tel-input";
 
@@ -8,40 +8,55 @@ const ContactForm = () => {
     fullName: "",
     phone: "",
     email: "",
+    request: "",
   });
 
+  const nameId = useId();
+  const phoneId = useId();
+  const emailId = useId();
+  const requestId = useId();
+
   return (
-    <form className="flex max-w-md flex-col gap-4">
+    <form className="flex max-w-md flex-col gap-4 bg-orange p-10 mt-10 mb-10 mr-auto ml-auto rounded-xl">
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="email1" value="Your email" />
+          <Label htmlFor={nameId} value="Full Name :" />
         </div>
-        <TextInput
-          id="email1"
-          type="email"
-          placeholder="name@flowbite.com"
-          required
-        />
+        <TextInput id={nameId} type="text" required />
       </div>
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="password1" value="Your password" />
+          <Label htmlFor={emailId} value="Email :" />
         </div>
-        <TextInput id="password1" type="password" required />
+        <TextInput id={emailId} type="email" required />
       </div>
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="password1" value="Your password" />
+          <Label htmlFor={phoneId} value="Phone :" />
         </div>
-        <Textarea id="password1" required />
-      </div>
-      <div className="flex items-center gap-2">
         <MuiTelInput
+          style={{
+            backgroundColor: "white",
+            borderRadius: "12px",
+            width: "100%",
+          }}
+          id={phoneId}
+          placeholder="Change country if necessary"
+          langOfCountryName="en"
+          size="small"
+          variant="outlined"
+          defaultCountry="CA"
           value={formData.phone}
           onChange={(value: any) => {
             updateFormData({ ...formData, phone: value });
           }}
         />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor={requestId} value="Request :" />
+        </div>
+        <Textarea id={requestId} maxLength={250} />
       </div>
       <Button type="submit">Submit</Button>
     </form>
