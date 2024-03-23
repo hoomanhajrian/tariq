1dfef063e7f1a83a67bf7c2b2250525fc270c3ed
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2024-03-23 10:34:10 -0700

    apos issue in services

--- src/app/en/services/page.tsx
+++ src/app/en/services/page.tsx
@@ -5,17 +5,17 @@ const Services = () => {
     <>
       <p>
         Tariq Louis offers a range of services for those interested in acquiring
-        his artwork or commissioning custom pieces. Whether you're a collector
-        looking to add a unique sculpture to your collection or an individual
-        seeking a personalized creation, Tariq is dedicated to bringing your
-        vision to life. Services include consultation sessions to discuss your
-        specific requirements, conceptualization and design of bespoke
-        sculptures, and expert craftsmanship to execute your project with
-        precision and skill. From small-scale sculptures to monumental
-        installations, Tariq's commitment to excellence ensures that each piece
-        is crafted with meticulous attention to detail and artistic integrity.
-        Contact us today to inquire about Tariq Louis's services and begin your
-        journey towards owning a masterpiece of sculptural art.
+        his artwork or commissioning custom pieces. Whether you&apos;re a
+        collector looking to add a unique sculpture to your collection or an
+        individual seeking a personalized creation, Tariq is dedicated to
+        bringing your vision to life. Services include consultation sessions to
+        discuss your specific requirements, conceptualization and design of
+        bespoke sculptures, and expert craftsmanship to execute your project
+        with precision and skill. From small-scale sculptures to monumental
+        installations, Tariq&apos;s commitment to excellence ensures that each
+        piece is crafted with meticulous attention to detail and artistic
+        integrity. Contact us today to inquire about Tariq Louis&apos;s services
+        and begin your journey towards owning a masterpiece of sculptural art.
       </p>
       <ContactForm />
     </>
