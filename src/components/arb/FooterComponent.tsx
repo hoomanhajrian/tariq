4ecaaff6bb9fdb461f71e47c3da9016e0f7cd761
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2024-03-27 12:18:02 -0700

    updated footer logo and styles

--- src/components/arb/FooterComponent.tsx
+++ src/components/arb/FooterComponent.tsx
@@ -4,25 +4,27 @@ import {
   FooterLink,
   FooterLinkGroup,
 } from "flowbite-react";
+import Image from "next/image";
+import Logo from "@/app/icon.png";
 export const FooterComponent = () => {
   const date = new Date();
   return (
     <Footer container className=" bg-green p-10" dir="rtl">
-      <FooterCopyright
-        className="font-extrabold text-textLight"
-        href="https://hh-portfolio.com"
-        by="Hooman"
-        year={date.getFullYear()}
-      />
-      <FooterLinkGroup>
+      <div className="flex flex-row">
+        <Image src={Logo} alt="tariq-logo" width={100} height={100} />
+        <h2 className="whitespace-nowrap text-3xl font-semibold text-white mt-8">
+          Tariq Louis
+        </h2>
+      </div>
+      <FooterLinkGroup className="gap-5">
         <FooterLink href="#" className="font-extrabold text-textLight">
-          هاتف : +1(778)-751-1620
+          هاتف : +1 (778)-751-1620
         </FooterLink>
         <FooterLink href="#" className="font-extrabold text-textLight">
           بريد إلكتروني : tariqlouiss@yahoo.com
         </FooterLink>
         <FooterLink href="#" className="font-extrabold text-textLight">
-          فيسبوك: tariq louis
+          فيسبوك : tariq louis
         </FooterLink>
       </FooterLinkGroup>
     </Footer>
--- src/components/en/FooterComponent.tsx
+++ src/components/en/FooterComponent.tsx
@@ -4,18 +4,26 @@ import {
   FooterLink,
   FooterLinkGroup,
 } from "flowbite-react";
+import Logo from "@/app/icon.png";
+import Image from "next/image";
 
 export const FooterComponent = () => {
   const date = new Date();
   return (
     <Footer container className="bg-green p-10">
-      <FooterCopyright
-        className="font-extrabold text-textLight"
-        href="https://hh-portfolio.com"
-        by="Hooman"
-        year={date.getFullYear()}
-      />
-      <FooterLinkGroup>
+      <div className="flex flex-row">
+        <Image src={Logo} alt="tariq-logo" width={100} height={100} />
+        <h2 className="whitespace-nowrap text-3xl font-semibold text-white mt-8">
+          Tariq Louis
+        </h2>
+      </div>
+      <FooterLinkGroup className="gap-5">
+        <FooterCopyright
+          className="font-extrabold text-textLight"
+          href="https://hh-portfolio.com"
+          by="Hooman"
+          year={date.getFullYear()}
+        />
         <FooterLink href="#" className="font-extrabold text-textLight">
           Phone: +1(778)-751-1620
         </FooterLink>
