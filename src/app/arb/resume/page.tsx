8eaea77d3eb6e2cc6ec5a0fa6d1cd3ee8452d0cb
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2024-03-30 17:28:44 -0700

    updated resume pages

--- src/app/arb/resume/page.tsx
+++ src/app/arb/resume/page.tsx
@@ -6,84 +6,211 @@ import {
   ListItem,
   ListItemText,
 } from "@mui/material";
+import Image from "next/image";
+import tariq from "@/pics/tariq.jpg";
 
 const Resume = () => {
   return (
-    <Container maxWidth="md" style={{ direction: "rtl" }}>
-      <Box my={4}>
-        <Typography variant="h3" component="h1" gutterBottom>
-          طارق لویس
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          نحات
-        </Typography>
-        <Typography variant="h5" component="h3" gutterBottom>
-          عضو جمعية الفنانين التشكيليين العراقيين
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          بريد إلكتروني : tariqlouiss@yahoo.com
-        </Typography>
-        <Box mt={4}>
-          <Typography variant="h4" gutterBottom>
-            تعليم
-          </Typography>
-          <List>
-            <ListItem>
-              <ListItemText
-                primary="Bachelor of Science in Computer Science"
-                secondary="University of Example, 2015-2019"
-              />
-            </ListItem>
-          </List>
+    <Container
+      maxWidth="md"
+      className="grid sm:grid-cols-1 md:grid-cols-2 text-right"
+    >
+      <Box my={4} className="md:col-span-2">
+        <Box className="flex flex-wrap justify-around bg-orange rounded-xl p-2 m-1 ml-0 mb-0">
+          <Box>
+            <Typography variant="h3" component="h1" gutterBottom>
+              طارق لويس جزراوی
+            </Typography>
+            <Typography variant="h5" component="h2" gutterBottom>
+              النحات والرسام
+            </Typography>
+            <Typography variant="h5" component="h3" gutterBottom>
+              عضو جمعية الفنانين التشكيليين العراقيين
+            </Typography>
+          </Box>
+          <Image
+            src={tariq}
+            width={100}
+            height={150}
+            alt="tariq louis jazwari"
+          />
         </Box>
-
-        <Box mt={4}>
-          <Typography variant="h4" gutterBottom>
-            Experience
+      </Box>
+      <Box my={4} className="md:col-span-2">
+        <Box>
+          <Typography variant="h6" component="p" gutterBottom>
+            أحب العمل بيدي، النجارة والبناء، أحب الناس وفني هو رمز حبي للناس.
+            ينصب تركيزي على الإنسان وأعطي كل قطعة فنية قطعة من روحي. الفن يعيش
+            في قلبي وروحي وأريد أن أظهر للعالم الآثار العراقية. لدي مجموعة كبيرة
+            من الآثار العراقية المقلدة وأريد أن يصبح الفن العراقي معروفاً في
+            مجتمعنا.
           </Typography>
-          <List>
-            <ListItem>
-              <ListItemText
-                primary="Frontend Developer"
-                secondary="ABC Company, 2019-Present"
-              />
-              <List>
-                <ListItem>
-                  <ListItemText primary="Developed responsive web applications using React.js" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Collaborated with design and backend teams to implement UI/UX designs" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Optimized application performance for better user experience" />
-                </ListItem>
-              </List>
-            </ListItem>
-          </List>
         </Box>
-
-        <Box mt={4}>
-          <Typography variant="h4" gutterBottom>
-            Skills
-          </Typography>
-          <List>
-            <ListItem>
-              <ListItemText primary="HTML5/CSS3" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="JavaScript (ES6+)" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="React.js" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Responsive Web Design" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Version Control (Git)" />
-            </ListItem>
-          </List>
-        </Box>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          تعليم
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="دبلومة معهد الفنون الجميلة"
+              secondary="جامعة بغداد – قسم النحت (العراقي) 1978"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="عضو تنفيذي"
+              secondary="جمعية الفنانين التشكيليين العراقيين 1978"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="دراسات في اللاهوت"
+              secondary="جامعة بغداد – قسم النحت (العراقية) 1980"
+            />
+          </ListItem>
+        </List>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          خبرة
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="العديد من الإنجازات الفنية في متحف تقاليد الأزياء الشعبية."
+              secondary="العراق، 1968-1970"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="عضو تنفيذي في جمعية الفنانين التشكيليين العراقيين."
+              secondary="العراق، 1978"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="معرض مشارك للفنانين العراقيين."
+              secondary="العراق (معرض تموز – عمان) 1993"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="إنجاز عمل نصب القديس هارديني مصنوع من البرونز طوله ١٧٠"
+              secondary="بيروت - لبنان، 2002"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="العديد من الإنجازات الفنية في متحف تقاليد الأزياء الشعبية."
+              secondary="العراق، 1968-1970"
+            />
+          </ListItem>
+        </List>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          المعارض
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary='معرض تموز: عرض عمل فني واحد "المقاومة" بمشاركة 300 فنان'
+              secondary="العراق"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="معرض للفنانين اللاجئين"
+              secondary="المكتبة المركزية في فانكوفر"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="معرض للفنانين اللاجئين"
+              secondary="SFU"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="الاجتماع السنوي AMSSA"
+              secondary="فانكوفر"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="معرض للفنانين اللاجئين حسب الخيارات"
+              secondary="بلدية"
+            />
+          </ListItem>
+        </List>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          مهارات
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText sx={{ textAlign: "right" }} primary="نجارة" />
+          </ListItem>
+          <ListItem>
+            <ListItemText sx={{ textAlign: "right" }} primary="النحت" />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="أخلاقيات العمل القوية"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText sx={{ textAlign: "right" }} primary="إدارة الوقت" />
+          </ListItem>
+          <ListItem>
+            <ListItemText sx={{ textAlign: "right" }} primary="بناء" />
+          </ListItem>
+          <ListItem>
+            <ListItemText sx={{ textAlign: "right" }} primary="إعداد المعرض" />
+          </ListItem>
+        </List>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          ابقى على تواصل
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="البريد الإلكتروني : tariqlouiss@yahoo.com"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="الهاتف : +1(778)-751-1620"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              sx={{ textAlign: "right" }}
+              primary="الفيسبوك: طارق لويس"
+            />
+          </ListItem>
+        </List>
       </Box>
     </Container>
   );
--- src/app/en/resume/page.tsx
+++ src/app/en/resume/page.tsx
@@ -6,84 +6,189 @@ import {
   ListItem,
   ListItemText,
 } from "@mui/material";
+import Image from "next/image";
+import tariq from "@/pics/tariq.jpg";
 
 const Resume = () => {
   return (
-    <Container maxWidth="md">
-      <Box my={4}>
-        <Typography variant="h3" component="h1" gutterBottom>
-          Tariq Louis
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          Sculptor
-        </Typography>
-        <Typography variant="h5" component="h3" gutterBottom>
-          Member Of Iraqi Plastic Artists
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          Email : tariqlouiss@yahoo.com
-        </Typography>
-        <Box mt={4}>
-          <Typography variant="h4" gutterBottom>
-            Education
-          </Typography>
-          <List>
-            <ListItem>
-              <ListItemText
-                primary="Bachelor of Science in Computer Science"
-                secondary="University of Example, 2015-2019"
-              />
-            </ListItem>
-          </List>
+    <Container maxWidth="md" className="grid sm:grid-cols-1 md:grid-cols-2">
+      <Box my={4} className="md:col-span-2">
+        <Box className="flex flex-wrap justify-around bg-orange rounded-xl p-2 m-1 ml-0 mb-0">
+          <Box>
+            <Typography
+              className="sm:text-md"
+              variant="h3"
+              component="h1"
+              gutterBottom
+            >
+              Tariq Louis Jazrawi
+            </Typography>
+            <Typography variant="h5" component="h2" gutterBottom>
+              Sculptor and Painter
+            </Typography>
+            <Typography variant="h5" component="h3" gutterBottom>
+              Member Of Iraqi Plastic Artists
+            </Typography>
+          </Box>
+          <Image
+            src={tariq}
+            width={100}
+            height={150}
+            alt="tariq louis jazwari"
+          />
         </Box>
-
-        <Box mt={4}>
-          <Typography variant="h4" gutterBottom>
-            Experience
+      </Box>
+      <Box my={4} className="md:col-span-2">
+        <Box>
+          <Typography variant="h6" component="p" gutterBottom>
+            I love working with my hands, carpentry and construction, I love
+            people and my art is my token of love to people. My focus is on the
+            human and I give every piece of art a piece of my soul. Art lives in
+            my heart and soul and I want to show the world Iraqi antiquities. I
+            have a large collection of imitations of Iraqi Antiquities and want
+            Iraqi art to become known in our community.
           </Typography>
-          <List>
-            <ListItem>
-              <ListItemText
-                primary="Frontend Developer"
-                secondary="ABC Company, 2019-Present"
-              />
-              <List>
-                <ListItem>
-                  <ListItemText primary="Developed responsive web applications using React.js" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Collaborated with design and backend teams to implement UI/UX designs" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Optimized application performance for better user experience" />
-                </ListItem>
-              </List>
-            </ListItem>
-          </List>
         </Box>
-
-        <Box mt={4}>
-          <Typography variant="h4" gutterBottom>
-            Skills
-          </Typography>
-          <List>
-            <ListItem>
-              <ListItemText primary="HTML5/CSS3" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="JavaScript (ES6+)" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="React.js" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Responsive Web Design" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Version Control (Git)" />
-            </ListItem>
-          </List>
-        </Box>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          Education
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText
+              primary="Diploma Frim The Fine Arts Institute"
+              secondary="Baghdad University ,Sculpture Dept (Iraqi), 1978"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Performing Member"
+              secondary="Iraqi Plastic Artists Society, 1978"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Studies in Theology"
+              secondary="Baghdad University ,Sculpture Dept (Iraqi), 1980"
+            />
+          </ListItem>
+        </List>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          Experience
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText
+              primary="Many Artistic Achivements in Popular Fashion Tradition Museum."
+              secondary="Iraq, 1968-1970"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Performing Member of The Iraqi Plastic Artists Society."
+              secondary="Iraq, 1978"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Participated Exhibition for Iraqi Artists."
+              secondary="Iraq (Tammouz fair - Amman), 1993"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Fulfill The Work Monument of Saint Hardini Made of Bronze 170 Long"
+              secondary="Beirut, Lebanon, 2002"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Many Artistic Achivements in Popular Fashion Tradition Museum."
+              secondary="Iraq, 1968-1970"
+            />
+          </ListItem>
+        </List>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          Exhibitions
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText
+              primary='Tammouz Exhibition: Displayed One Artwork "Resistance" in a 300 artist'
+              secondary="Iraq, Baghdad"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Exhibition for Refugee Artists"
+              secondary="Central Library Vancouver"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Exhibition for Refugee Artists"
+              secondary="SFU"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="AMSSA Annual Meeting"
+              secondary="Vancouver"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Exhibition for Refugee Artists by Options"
+              secondary="City Hall"
+            />
+          </ListItem>
+        </List>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          Skills
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText primary="Carpentry" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Sculpture" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Strong Work Ethic" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Time Management" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Construction" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Exhibition Setup" />
+          </ListItem>
+        </List>
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h4" gutterBottom>
+          Get in Touch
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText primary="Email : tariqlouiss@yahoo.com" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Phone : +1(778)-751-1620" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Facebook : Tariq Louis" />
+          </ListItem>
+        </List>
       </Box>
     </Container>
   );
