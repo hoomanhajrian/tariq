26614a505b0f1a4d8a47076a8e29da68d76c6f71
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2024-04-05 21:51:13 -0700

    using google storage for photoes

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,9 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: "https",
-                hostname: "images.unsplash.com",
-            },
+                protocol: 'https',
+                hostname: 'storage.googleapis.com'
+            }
         ],
     },
 };
