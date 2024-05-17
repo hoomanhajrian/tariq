d1dd9a573b9d49998f82f10bb6c82657b302ccd1
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-05-17 22:46:11 +0000

    updated email files

--- src/app/api/mail/send/route.ts
+++ src/app/api/mail/send/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 // import { emailLimiter } from "../limiter";
 // import { EmailTemplate } from "../EmailTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   const res = await req.json();
